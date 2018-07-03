4609eb9a3bd40cc116a397d3c45d3fc5d91f338c
Leo <leonardjonathanoh@live.com>
2018-07-03 21:31:49 +0800

    Update examples

--- Example.ps1
+++ Example.ps1
@@ -1,8 +1,14 @@
+cd $PSScriptRoot
+
+. ./SourceQuery.ps1
+. ./GoldSourceRcon.ps1
+. ./SourceRcon.ps1
+
 # Examples
 SourceQuery '127.0.0.1' '27015' 'info'
 SourceQuery '127.0.0.1' '27015' 'players'
 SourceQuery '127.0.0.1' '27015' 'rules'
 
 SourceRcon '127.0.0.1' '27015' 'rcon_password' 'status'
-
 GoldSourceRcon '127.0.0.1' '27015' 'rcon_password' 'status'
+
