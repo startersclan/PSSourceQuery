4bd9907eefa36c88bcd9bd6b1a8f927cf4007421
Leo <leonardjonathanoh@live.com>
2018-07-05 00:28:23 +0800

    Update example

--- Example.ps1
+++ Example.ps1
@@ -4,12 +4,16 @@ cd $PSScriptRoot
 . ./GoldSourceRcon.ps1
 . ./SourceRcon.ps1
 
-# Examples
-SourceQuery '127.0.0.1' '27015' 'info'
-SourceQuery '127.0.0.1' '27015' 'players'
-SourceQuery '127.0.0.1' '27015' 'rules'
-SourceQuery '127.0.0.1' '27015' 'ping'
+SourceQuery '127.0.0.1' '27015' 'goldsource' 'info'
+SourceQuery '127.0.0.1' '27015' 'goldsource' 'players'
+SourceQuery '127.0.0.1' '27015' 'goldsource' 'rules'
+SourceQuery '127.0.0.1' '27015' 'goldsource' 'ping'
 
-#SourceRcon '127.0.0.1' '27015' 'rcon_password' 'status'
+SourceQuery '127.0.0.1' '27015' 'source' 'info'
+SourceQuery '127.0.0.1' '27015' 'source' 'players'
+SourceQuery '127.0.0.1' '27015' 'source' 'rules'
+SourceQuery '127.0.0.1' '27015' 'source' 'ping'
+
+SourceRcon '127.0.0.1' '27015' 'rcon_password' 'status'
 GoldSourceRcon '127.0.0.1' '27015' 'rcon_password' 'status'
 
